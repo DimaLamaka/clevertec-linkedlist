05e0baa9d109148b58a704e57aa1c51c9abccc41
Dzmitry Lamaka <lamaka.dima@gmail.com>
2022-03-25 12:35:29 +0300

    add MyLinkedList

---
+++ src/main/java/ru/clevertec/list/MyList.java
@@ -0,0 +1,14 @@
+package ru.clevertec.list;
+
+public interface MyList<T> {
+    T get(int index);
+
+    void set(int index, T t);
+
+    void add(T t);
+
+    void add(int index, T t);
+
+    void remove(T t);
+
+}
